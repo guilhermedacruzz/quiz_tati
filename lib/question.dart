a4f3a6f82c7a0249e610966b2da00b983cd612ef
indexGuilherme <guidacruz2004@gmail.com>
2022-06-25 11:27:45 -0300

    criando o objeto das questões

---
+++ lib/question.dart
@@ -0,0 +1,12 @@
+class Question {
+ final String text;
+ final String correct;
+ final List allOptions;
+ 
+ Question({
+   required this.text,
+   required this.correct,
+   required this.allOptions
+ });
+ 
+}
