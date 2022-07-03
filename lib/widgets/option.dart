94cb57fc90bcb3d2d953f06ddf568fcb8fba873f
indexGuilherme <guidacruz2004@gmail.com>
2022-07-03 18:25:25 -0300

    cara sei la

--- lib/widgets/option.dart
+++ lib/widgets/option.dart
@@ -1,6 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_tati/widgets/optionNormal.dart';
-import 'package:quiz_tati/widgets/optionSelected.dart';
+
+class OptionNormal extends StatelessWidget {
+  final String option;
+  final String index;
+
+  const OptionNormal(
+      {Key? key,
+      required this.option,
+      required this.index})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          decoration: BoxDecoration(
+            color: const Color(0xfffca311),
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(0, 2),
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: Text(
+            index,
+            style: const TextStyle(
+              color: Color(0xffffffff),
+              fontSize: 20,
+            ),
+          ),
+        ),
+        const SizedBox(width: 40),
+        Text(
+          option,
+          style: const TextStyle(
+            color: Color(0xff2b2d42),
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class OptionSelected extends StatelessWidget {
+  final String option;
+  final bool isCorrect;
+
+  const OptionSelected({Key? key, required this.option, required this.isCorrect }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          option,
+          style: const TextStyle(
+            color: Color(0xff2b2d42),
+            fontSize: 14,
+          ),
+        ),
+        const Spacer(),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          decoration: BoxDecoration(
+            color: const Color(0xffffffff),
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(0, 2),
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: Text(
+            isCorrect ? "X":"V",
+            style: const TextStyle(
+              color: Color(0xfffca311),
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 class Option extends StatelessWidget {
   final String option;
@@ -24,15 +114,16 @@ class Option extends StatelessWidget {
       onTap: () {
         selectQuestion(option);
       },
-      child: isSelected
-          ? OptionSelected(
-              option: option,
-              isCorret: isCorrect,
-            )
-          : OptionNormal(
-              option: option,
-              index: index,
-            ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+        margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 8),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: isSelected ?const Color(0xfffca311):const Color(0xffffffff),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: isSelected ? OptionSelected(option: option, isCorrect: isCorrect):OptionNormal(option: option, index: index)
+      ),
     );
   }
 }
