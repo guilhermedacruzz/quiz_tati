4dd3f06efc4f8a920daec07ed76a103966726ee2
indexGuilherme <guidacruz2004@gmail.com>
2022-07-03 20:36:59 -0300

    colocando as perguntas que o professor queria

---
+++ lib/data/data.dart
@@ -0,0 +1,37 @@
+List<Map> groupQuestions = [
+  {
+    "text": "Qual a capital do Brasil?",
+    "correct": "Brasília",
+    "otherOptions": ["São Paulo", "Rio de Janeiro", "Curitiba"]
+  },
+  {
+    "text": "Qual a capital do Paraná?",
+    "correct": "Curitiba",
+    "otherOptions": ["São Paulo", "Rio de Janeiro", "Porto Alegre"]
+  },
+  {
+    "text": "Onde está localizado o Brasil?",
+    "correct": "América do Sul",
+    "otherOptions": ["Africa", "Europa", "Ásia"]
+  },
+  {
+    "text": "Em que ano o Brasil foi encontrado?",
+    "correct": "1500",
+    "otherOptions": ["1800", "1200", "1900"]
+  },
+  {
+    "text": "Em que ano a seleção brasileira conquistou o tetra?",
+    "correct": "1994",
+    "otherOptions": ["1995", "1990", "1998"]
+  },
+  {
+    "text": "Qual o principal ingrediente do omelete?",
+    "correct": "Ovo",
+    "otherOptions": ["Queijo", "Manteiga", "Farinha"]
+  },
+  {
+    "text": "Qual a temperatura de ebulição da água a 1 atm?",
+    "correct": "100 ºC",
+    "otherOptions": ["50 ºC", "150 ºC", "95 ºC"]
+  },
+];
--- lib/repository/repositoryQuestions.dart
+++ lib/repository/repositoryQuestions.dart
@@ -1,6 +1,33 @@
+import 'package:quiz_tati/data/data.dart';
+
 import '../models/question.dart';
 import '../data/generator.dart';
 
+class QuestionRepository {
+  List<Question> generate(int qtde) {
+    List<Question> questions = [];
+
+    groupQuestions.shuffle();
+
+    for (int i = 0; i < qtde; i++) {
+      Map dataQuestion = groupQuestions[i];
+
+      var question = Question(
+          text: dataQuestion["text"],
+          correct: dataQuestion["correct"],
+          allOptions: [
+            ...dataQuestion["otherOptions"], 
+            dataQuestion["correct"]]
+          ..shuffle());
+
+      questions.add(question);
+    }
+
+    return questions;
+  }
+}
+
+/*
 class QuestionRepository {
   List<Question> generate(int qtde) {
     List<Question> questions = [];
@@ -21,4 +48,4 @@ class QuestionRepository {
 
     return questions;
   }
-}
+}*/
